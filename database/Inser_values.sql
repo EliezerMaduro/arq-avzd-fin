df18c84a7c6edc30961962e9a45787ef3faabeef
Eliezer Maduro <eliezer.maduro@gmail.com>
2023-09-22 16:30:54 -0500

    Update DB scripts

--- database/Inser_values.sql
+++ database/Inser_values.sql
@@ -1,91 +1,98 @@
--- Insertar datos en la tabla de Factura de Venta
-INSERT INTO sales_invoice (invoice_id, invoice_number, issue_date, customer_id, quantity, subtotal, discounts, taxes, total, payment_method, payment_status, currency, exchange_rate)
-VALUES
-    (1, 'INV-2023-001', '2023-09-20', 1, 3, 29.97, 0.00, 5.39, 35.36, 'Tarjeta de crédito', 'Pagado', 'USD', 1.0000),
-    (2, 'INV-2023-002', '2023-09-21', 2, 2, 31.98, 2.00, 5.76, 35.74, 'Efectivo', 'Pendiente', 'USD', 1.0000);
-
--- Insertar datos en la tabla de invoice_item (Detalles de factura)
-INSERT INTO invoice_item (invoice_item_id, invoice_id, product_id)
-VALUES
-    (1, 1, 1),
-    (2, 1, 2),
-    (3, 2, 1);
-
--- Insertar datos en la tabla de Ingresos
-INSERT INTO income (income_id, income_date, invoice_id, income_source, amount, description, payment_method, income_status, currency, exchange_rate, income_category)
-VALUES
-    (1, '2023-09-20', 1, 'INV-2023-001', 35.36, 'Ingreso por venta de productos', 'Tarjeta de crédito', 'Cobrado', 'USD', 1.0000, 'Ventas'),
-    (2, '2023-09-22', 2, 'INV-2023-002', 0.00, 'Pago pendiente de factura', 'Efectivo', 'Pendiente', 'USD', 1.0000, 'Cuentas por cobrar');
-
--- Insertar datos en la tabla de Comprobante de Egresos
-INSERT INTO expense_receipt (receipt_id, receipt_number, issue_date, expense_category, amount, payment_method, payment_status, currency, exchange_rate)
-VALUES
-    (1, 'REC-2023-001', '2023-09-20', 'Suministros de Oficina', 50.00, 'Tarjeta de crédito', 'Pagado', 'USD', 1.0000),
-    (2, 'REC-2023-002', '2023-09-21', 'Alquiler de Oficina', 1000.00, 'Transferencia bancaria', 'Pagado', 'USD', 1.0000);
-
--- Insertar datos en la tabla de Costos
-INSERT INTO costs (cost_id, cost_date, cost_category, amount, description)
-VALUES
-    (1, '2023-09-20', 'Materiales de Producción', 200.00, 'Compra de materias primas'),
-    (2, '2023-09-22', 'Servicios Profesionales', 500.00, 'Honorarios legales');
-
--- Insertar datos en la tabla de Gastos
-INSERT INTO expenses (expense_id, center_id, expense_date, expense_category, amount, description)
-VALUES
-    (1, 1, '2023-09-20', 'Suministros de Oficina', 50.00, 'Compra de suministros para la oficina'),
-    (2, 2, '2023-09-22', 'Alquiler de Oficina', 1000.00, 'Pago mensual de alquiler');
-
--- Insertar datos en la tabla de Accounting Accounts
-INSERT INTO accounting_accounts (account_id, account_name, account_type, amount)
-VALUES
-    (1, 'Cuenta de Activos', 'Activo', 10000.00),
-    (2, 'Cuenta de Pasivos', 'Pasivo', 5000.00),
-    (3, 'Cuenta de Patrimonio', 'Patrimonio', 3000.00);
-
--- Insertar datos en la tabla de Movimientos
-INSERT INTO transactions (transaction_id, transaction_date, description, amount)
-VALUES
-    (1, '2023-09-20', 'Compra de suministros', -200.00),
-    (2, '2023-09-21', 'Venta de productos', 1000.00);
-
--- Insertar datos en la tabla de Centro de Costos
-INSERT INTO cost_centers (center_id, center_name, description)
-VALUES
-    (1, 'Centro de Costo A', 'Descripción del Centro de Costo A'),
-    (2, 'Centro de Costo B', 'Descripción del Centro de Costo B');
-
--- Insertar datos en la tabla de Responsable
-INSERT INTO responsibles (responsible_id, responsible_name, contact_info)
-VALUES
-    (1, 'Juan Pérez', 'Correo: juan@example.com, Teléfono: 1234567890'),
-    (2, 'Ana Gómez', 'Correo: ana@example.com, Teléfono: 9876543210');
-
--- Insertar datos en la tabla de Activos
-INSERT INTO assets (asset_id, asset_name, purchase_date, purchase_price, description)
-VALUES
-    (1, 'Computadora', '2023-09-15', 1200.00, 'Computadora de escritorio'),
-    (2, 'Vehículo', '2023-09-18', 15000.00, 'Automóvil para la empresa');
-
--- Insertar datos en la tabla de Pasivos
-INSERT INTO liabilities (liability_id, liability_name, due_date, amount, description)
-VALUES
-    (1, 'Préstamo Bancario', '2023-10-01', 5000.00, 'Préstamo para capital de trabajo'),
-    (2, 'Facturas por Pagar', '2023-09-30', 2000.00, 'Facturas pendientes de pago');
-
--- Insertar datos en la tabla de Patrimonio
-INSERT INTO patrimony (assets_id, assets_name, amount, description)
-VALUES
-    (1, 'Capital Inicial', 10000.00, 'Inversión inicial de los socios'),
-    (2, 'Utilidades Acumuladas', 2500.00, 'Ganancias acumuladas');
-
--- Insertar datos en la tabla de clientes
-INSERT INTO customers (customer_id, name, last_name, email, phone_number, adress)
-VALUES
-    (1, 'Juan', 'Perez', 'juan@example.com', 1234567890, '123 Main St'),
-    (2, 'Ana', 'Gomez', 'ana@example.com', 9876543210, '456 Elm St');
-
 -- Insertar datos en la tabla de productos
-INSERT INTO products (product_id, product_name, product_price)
+INSERT INTO products (product_name, product_price)
 VALUES
-    (1, 'Producto 1', 10.99),
-    (2, 'Producto 2', 15.99);
+    ('Papa', 10.99),
+    ('Café', 15.99),
+   	('Aguacate', 6.00),
+  	('Platano', 4.50);
+    
+-- Insertar datos en la tabla de clientes
+INSERT INTO customers (name, last_name, email, phone_number, adress)
+VALUES
+    ('Juan', 'Perez', 'juan@example.com', '1234567890', '123 Main St'),
+    ('Ana', 'Gomez', 'ana@example.com', '9876543210', '456 Elm St');
+    
+-- Insertar datos en la tabla de Patrimonio
+INSERT INTO equity (month, year, date, amount, description)
+VALUES
+    (1, 2023, '2023-09-22' ,1109500.00, 'Inversión inicial de los socios'),
+    (2, 2023, '2023-09-22' ,1271830.00, 'Ganancias acumuladas');
+    
+-- Insertar datos en la tabla de Pasivos
+INSERT INTO liabilities (liability_name, due_date, amount, description)
+VALUES
+    ('Préstamo Bancario', '2023-10-01', 5000.00, 'Préstamo para capital de trabajo'),
+    ('Facturas por Pagar', '2023-09-30', 2000.00, 'Facturas pendientes de pago');
+    
+-- Insertar datos en la tabla de Activos
+INSERT INTO assets (asset_name, purchase_date, value, description)
+VALUES
+    ('Computadora', '2023-09-15', 1200.00, 'Computadora de escritorio'),
+    ('Vehículo', '2023-09-18', 15000.00, 'Automóvil para la empresa'),
+    ('Tractor', '2023-07-18', 750000.00, 'Tractor para el campo'),
+    ('Terreno', '2023-05-18', 3500000.00, 'Tractor para el campo'),
+    ('Caja', '2023-05-18', 20000.00, 'Tractor para el campo');
+    
+-- Insertar datos en la tabla de Responsable
+INSERT INTO responsibles (responsible_name, contact_info)
+VALUES
+    ('Eliezer Maduro', 'Correo: eliezer.maduro@evergreen.com, Teléfono: 1234567890'),
+    ('Juan Camilo Echeverry', 'Correo: juanc.echeverry@evergreen.com, Teléfono: 9876543210');
+    
+-- Insertar datos en la tabla de Centro de Costos
+INSERT INTO cost_centers (center_name, description)
+VALUES
+    ('Centro de Costo GRANJA', 'Centro de costos para la granja'),
+    ('Centro de Costo SUMINISTRO', 'Centro de costos para suministros');
+    
+-- Insertar datos en la tabla de Movimientos
+INSERT INTO transactions (transaction_date,transaction_type, description, amount)
+VALUES
+    ('2023-09-20', 'COMPRA' ,'Compra de suministros', 200.00),
+    ('2023-09-21', 'VENTA' ,'Venta de productos', 1000.00);
+    
+-- Insertar datos en la tabla de Accounting Accounts
+INSERT INTO accounting_accounts (account_name, account_type, transaction_id, date, amount)
+VALUES
+    ('Egreso por compra de materiales', 'Pasivo', 1 , '2023-09-20', 200.00),
+    ('Ingreso por venta', 'Activo', 2, '2023-09-21', 1000.00);
+    
+-- Insertar datos en la tabla de Gastos
+INSERT INTO expenses (center_id, expense_date, expense_category, responsible_id, amount, description)
+VALUES
+    (1, '2023-09-20', 'Suministros de Oficina', 1, 50.00, 'Compra de suministros para la oficina'),
+    (2, '2023-09-22', 'Alquiler de Oficina', 2, 1000.00, 'Pago mensual de alquiler');
+   
+select * from expenses;
+    
+-- Insertar datos en la tabla de Costos
+INSERT INTO costs (cost_date, cost_category, amount, description)
+VALUES
+    ('2023-09-20', 'Materiales de Producción', 200.00, 'Compra de materias primas'),
+    ('2023-09-22', 'Servicios Profesionales', 500.00, 'Honorarios legales');
+    
+-- Insertar datos en la tabla de Comprobante de Egresos
+INSERT INTO expense_receipt (receipt_number, issue_date, expense_category, amount, payment_method, payment_status, currency, exchange_rate)
+VALUES
+    ('REC-2023-001', '2023-09-20', 'Suministros de Oficina', 50.00, 'Tarjeta de crédito', 'Pagado', 'USD', 1.0000),
+    ('REC-2023-002', '2023-09-21', 'Alquiler de Oficina', 1000.00, 'Transferencia bancaria', 'Pagado', 'USD', 1.0000);
+   
+-- Insertar datos en la tabla de Factura de Venta
+INSERT INTO sales_invoice (invoice_number, issue_date, customer_id, subtotal, discounts, taxes, total, payment_method, payment_status, currency, exchange_rate)
+VALUES
+    ('INV-2023-001', '2023-09-20', 1, 29.97, 0.00, 5.39, 35.36, 'Tarjeta de crédito', 'Pagado', 'USD', 1.0000),
+    ('INV-2023-002', '2023-09-21', 2, 31.98, 2.00, 5.76, 35.74, 'Efectivo', 'Pendiente', 'USD', 1.0000);
+    
+-- Insertar datos en la tabla de Ingresos
+INSERT INTO income (income_date, invoice_id, income_source, amount, description, payment_method, income_status, currency, exchange_rate, income_category)
+VALUES
+    ('2023-09-20', 1, 'INV-2023-001', 35.36, 'Ingreso por venta de productos', 'Tarjeta de crédito', 'Cobrado', 'USD', 1.0000, 'Ventas'),
+    ('2023-09-22', 2, 'INV-2023-002', 0.00, 'Pago pendiente de factura', 'Efectivo', 'Pendiente', 'USD', 1.0000, 'Cuentas por cobrar');
+    
+-- Insertar datos en la tabla de invoice_item (Detalles de factura)
+INSERT INTO invoice_item (invoice_id, product_id, quantity)
+VALUES
+    (1, 3, 100),
+    (1, 4, 25),
+    (2, 5, 75),
+    (2, 6, 60);
--- database/querys_bd.sql
+++ database/querys_bd.sql
@@ -1,10 +1,123 @@
+-- Tabla de clientes
+CREATE TABLE customers (
+    customer_id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50),
+    phone_number VARCHAR(20),
+    adress VARCHAR(200)
+);
+
+-- Tabla de productos
+CREATE TABLE  products (
+    product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(50),
+    product_price  NUMERIC(12, 2)
+);
+
+-- Tabla de Responsable
+CREATE TABLE responsibles (
+    responsible_id SERIAL PRIMARY KEY,
+    responsible_name VARCHAR(255),
+    contact_info TEXT
+);
+
+-- Tabla de Activos
+CREATE TABLE assets (
+    asset_id SERIAL PRIMARY KEY,
+    asset_name VARCHAR(255),
+    purchase_date DATE,
+    value NUMERIC(12, 2),
+    description TEXT
+);
+
+-- Tabla de Pasivos
+CREATE TABLE liabilities (
+    liability_id SERIAL PRIMARY KEY,
+    liability_name VARCHAR(255),
+    due_date DATE,
+    amount NUMERIC(12, 2),
+    description TEXT
+);
+
+-- Tabla de Patrimonio
+CREATE TABLE equity (
+    equity_id SERIAL PRIMARY KEY,
+    month INTEGER,
+    year INTEGER,
+    date DATE,
+    amount NUMERIC(12, 2),
+    description TEXT
+);
+
+-- Tabla de Movimientos
+CREATE TABLE transactions (
+    transaction_id SERIAL PRIMARY KEY,
+    transaction_type VARCHAR(10),
+    transaction_date DATE,
+    description TEXT,
+    amount NUMERIC(12, 2)
+);
+
+-- Tabla de Centro de Costos
+CREATE TABLE cost_centers (
+    center_id SERIAL PRIMARY KEY,
+    center_name VARCHAR(255),
+    description TEXT
+);
+
+-- Tabla de Costos
+CREATE TABLE costs (
+    cost_id SERIAL PRIMARY KEY,
+    cost_date DATE,
+    cost_category VARCHAR(100),
+    amount NUMERIC(12, 2),
+    description TEXT
+);
+
+-- Tabla de Gastos
+create table expenses (
+    expense_id SERIAL PRIMARY KEY,
+    center_id SERIAL, 
+    expense_date DATE,
+    expense_category VARCHAR(100),
+    responsible_id SERIAL,
+    amount NUMERIC(12, 2),
+    description TEXT,
+    FOREIGN KEY (center_id) REFERENCES cost_centers(center_id),
+    FOREIGN KEY (responsible_id) REFERENCES responsibles(responsible_id)
+);
+
+-- Tabla de Cuentas Contables
+CREATE TABLE accounting_accounts (
+    account_id SERIAL PRIMARY KEY,
+    account_name VARCHAR(255),
+    account_type VARCHAR(50),
+    transaction_id SERIAL,
+    date DATE,
+    amount NUMERIC(12,2),
+    FOREIGN KEY (transaction_id) REFERENCES transactions(transaction_id)
+);
+
+-- Tabla de Comprobante de Egresos
+CREATE TABLE expense_receipt (
+    receipt_id SERIAL PRIMARY KEY,
+    receipt_number VARCHAR(255),
+    issue_date DATE,
+    expense_category VARCHAR(100),
+    amount NUMERIC(12, 2),
+    payment_method VARCHAR(100),
+    payment_status VARCHAR(50),
+    currency VARCHAR(10),
+    exchange_rate NUMERIC(10, 4)
+);
+
 -- Tabla de Factura de Venta
 CREATE TABLE sales_invoice (
     invoice_id SERIAL PRIMARY KEY,
     invoice_number VARCHAR(255),
     issue_date DATE,
     customer_id INTEGER,
-    quantity INTEGER,
     subtotal NUMERIC(12, 2),
     discounts NUMERIC(12, 2),
     taxes NUMERIC(12, 2),
@@ -16,13 +129,6 @@ CREATE TABLE sales_invoice (
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
-CREATE TABLE invoice_item (
-    invoice_item_id SERIAL PRIMARY KEY,
-    invoice_id SERIAL,
-    product_id INTEGER
-    FOREIGN KEY (invoice_id) REFERENCES sales_invoice(invoice_id),
-    FOREIGN KEY (product_id) REFERENCES product(product_id)
-);
 
 -- Tabla de Ingresos
 CREATE TABLE income (
@@ -40,110 +146,12 @@ CREATE TABLE income (
     FOREIGN KEY (invoice_id) REFERENCES sales_invoice(invoice_id)
 );
 
--- Tabla de Comprobante de Egresos
-CREATE TABLE expense_receipt (
-    receipt_id SERIAL PRIMARY KEY,
-    receipt_number VARCHAR(255),
-    issue_date DATE,
-    expense_category VARCHAR(100),
-    amount NUMERIC(12, 2),
-    payment_method VARCHAR(100),
-    payment_status VARCHAR(50),
-    currency VARCHAR(10),
-    exchange_rate NUMERIC(10, 4)
-);
 
--- Tabla de Costos
-CREATE TABLE costs (
-    cost_id SERIAL PRIMARY KEY,
-    cost_date DATE,
-    cost_category VARCHAR(100),
-    amount NUMERIC(12, 2),
-    description TEXT
-);
-
--- Tabla de Gastos
-CREATE TABLE expenses (
-    expense_id SERIAL PRIMARY KEY,
-    center_id SERIAL, 
-    expense_date DATE,
-    expense_category VARCHAR(100),
-    amount NUMERIC(12, 2),
-    description TEXT,
-    FOREIGN KEY (center_id) REFERENCES cost_centers(center_id)
-);
-
-
--- Tabla de Cuentas Contables
-CREATE TABLE accounting_accounts (
-    account_id SERIAL PRIMARY KEY,
-    account_name VARCHAR(255),
-    account_type VARCHAR(50),
-    amount NUMERIC(12,2)
-);
-
-
--- Tabla de Movimientos
-CREATE TABLE transactions (
-    transaction_id SERIAL PRIMARY KEY,
-    transaction_date DATE,
-    description TEXT,
-    amount NUMERIC(12, 2),
-    
-);
-
--- Tabla de Centro de Costos
-CREATE TABLE cost_centers (
-    center_id SERIAL PRIMARY KEY,
-    center_name VARCHAR(255),
-    description TEXT
-);
-
--- Tabla de Responsable
-CREATE TABLE responsibles (
-    responsible_id SERIAL PRIMARY KEY,
-    responsible_name VARCHAR(255),
-    contact_info TEXT
-);
-
--- Tabla de Activos
-CREATE TABLE assets (
-    asset_id SERIAL PRIMARY KEY,
-    asset_name VARCHAR(255),
-    purchase_date DATE,
-    purchase_price NUMERIC(12, 2),
-    description TEXT
-);
-
--- Tabla de Pasivos
-CREATE TABLE liabilities (
-    liability_id SERIAL PRIMARY KEY,
-    liability_name VARCHAR(255),
-    due_date DATE,
-    amount NUMERIC(12, 2),
-    description TEXT
-);
-
--- Tabla de Patrimonio
-CREATE TABLE patrimony (
-    assets_id SERIAL PRIMARY KEY,
-    assets_name VARCHAR(255),
-    amount NUMERIC(12, 2),
-    description TEXT
-);
--- Tabla de clientes
-CREATE TABLE customers (
-    customer_id INTEGER PRIMARY KEY,
-    name VARCHAR(50),
-    last_name VARCHAR(50),
-    email VARCHAR(50),
-    phone_number NUMERIC,
-    adress VARCHAR(200)
-);
-
--- Tabla de productos
-CREATE TABLE  products (
-    product_id INTEGER PRIMARY KEY,
-    product_name VARCHAR(50),
-    product_price  NUMERIC(12, 2)
+CREATE TABLE invoice_item (
+    invoice_item_id SERIAL PRIMARY KEY,
+    invoice_id SERIAL,
+    product_id INTEGER,
+    quantity INTEGER,
+    FOREIGN KEY (invoice_id) REFERENCES sales_invoice(invoice_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
