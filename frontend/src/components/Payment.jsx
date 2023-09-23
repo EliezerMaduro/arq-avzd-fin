ccd8bd02439b611a2ea85f2a7b2603c37d0a363c
David Tibaduiza <davidet9412@gmail.com>
2023-09-22 23:31:05 -0500

    Changes in the purchase form

--- frontend/src/components/Payment.jsx
+++ frontend/src/components/Payment.jsx
@@ -21,14 +21,14 @@ const Payment = () => {
           <h2 className="text-uppercase text-center mb-5">Pay your order</h2>
           <form onSubmit={handleSubmit(onSubmit)}>
           <MDBInput {...register("invoice_number")} wrapperClass='mb-4' label='Invoice number' size='lg' id='form1' type='text'/>
-          <MDBInput {...register("payment_method")} wrapperClass='mb-4' label='Payment Method' size='lg' id='form1' type='text'/>
-          <MDBInput {...register("currency")} wrapperClass='mb-4' label='Currency' size='lg' id='form2' type='text'/>
+          <MDBInput {...register("payment_method")} wrapperClass='mb-4' label='Payment Method' size='lg' id='form1' type='text' disabled/>
+          <MDBInput {...register("currency")} wrapperClass='mb-4' label='Currency' size='lg' id='form2' type='text'disabled/>
           <h4 className="text text mb-5">Products</h4>
-          <MDBInput {...register("product")} wrapperClass='mb-4' label='Product(s)' size='lg' id='form1' type='text'/>
-          <MDBInput {...register("price")} wrapperClass='mb-4' label='Unit Price' size='lg' id='form1' type='number'/>
-          <MDBInput {...register("subtotal")} wrapperClass='mb-4' label='Subtotal' size='lg' id='form1' type='number'/>
-          <MDBInput {...register("taxes")} wrapperClass='mb-4' label='Taxes' size='lg' id='form1' type='number'/>
-          <MDBInput {...register("total")} wrapperClass='mb-4' label='Total' size='lg' id='form1' type='number'/>
+          <MDBInput {...register("product")} wrapperClass='mb-4' label='Product' size='lg' id='form1' type='text'disabled/>
+          <MDBInput {...register("price")} wrapperClass='mb-4' label='Unit Price' size='lg' id='form1' type='number' disabled/>
+          <MDBInput {...register("subtotal")} wrapperClass='mb-4' label='Subtotal' size='lg' id='form1' type='number'disabled/>
+          <MDBInput {...register("taxes")} wrapperClass='mb-4' label='Taxes' size='lg' id='form1' type='number'disabled/>
+          <MDBInput {...register("total")} wrapperClass='mb-4' label='Total' size='lg' id='form1' type='number'disabled/>
           <MDBBtn type="submit" className='mb-4 w-100 gradient-custom-4' size='lg'>Pay</MDBBtn>
           </form>
         </MDBCardBody>
--- frontend/src/components/Purchase.jsx
+++ frontend/src/components/Purchase.jsx
@@ -1,39 +1,227 @@
-import React from 'react';
-import {useForm} from 'react-hook-form'
+import { useEffect, useState } from "react";
 import {
-  MDBBtn,
-  MDBContainer,
-  MDBCard,
-  MDBCardBody,
-  MDBInput
-}
-from 'mdb-react-ui-kit';
+  FloatingLabel,
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  Row,
+} from "react-bootstrap";
+import { useForm, useFieldArray, set } from "react-hook-form";
 
-const Purchase = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = data => {
-    console.log(data)
-  }
+function Purchase() {
+  /*const mockClients = [
+    {
+      id: "1",
+      name: "David",
+      lastname: "Tibaduiza",
+      email: "davidet9412@gmail.com",
+      phone: "3012740485",
+      address: "Carrera 66 # 38 - 27",
+    },
+  ];*/
+  const [customers, setCustomers] = useState([]);
+  const { control, resetField, register, setValue, getValues, handleSubmit } =
+    useForm();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "products",
+  });
+  const onSubmit = (data) => {
+    delete data.product;
+    console.log(data);
+  };
+
+  const onHandleChange = (e) => {
+    const name = e.target.value;
+    const customer = customers.find((customer) => customer.name === name);
+    setValue("customer_id",customer.customer_id);
+    setValue("lastname", customer.last_name);
+    setValue("email", customer.email);
+    setValue("phone_number", customer.phone_number);
+    setValue("address", customer.address);
+  };
+
+  const onAddProduct = (product) => {
+    append(product);
+    resetField("product.name");
+    resetField("product.quantity");
+    resetField("product.value");
+  };
+
+  useEffect(() => {
+    setCustomers(customers);
+  }, []);
   return (
-    <MDBContainer fluid className='d-flex align-items-center justify-content-center bg-image' style={{height: "100%"}}>
-      <MDBCard className='m-5' style={{maxWidth: '600px'}}>
-        <MDBCardBody className='px-5'>
-          <h2 className="text-uppercase text-center mb-5">Create a purchase order</h2>
+    <Container
+      fluid
+      className="d-flex align-items-center justify-content-center bg-image"
+      style={{ height: "100%" }}
+    >
+      <Card className="m-5" style={{ maxWidth: "600px" }}>
+        <Card.Body className="px-5">
+          <h2 className="text-uppercase text-center mb-5">
+            Create a purchase order
+          </h2>
           <h4 className="text text mb-5">Your information</h4>
           <form onSubmit={handleSubmit(onSubmit)}>
-          <MDBInput {...register("name")} wrapperClass='mb-4' label='Name' size='lg' id='form1' type='text'/>
-          <MDBInput {...register("last_name")} wrapperClass='mb-4' label='Last name' size='lg' id='form1' type='text'/>
-          <MDBInput {...register("email")} wrapperClass='mb-4' label='Your Email' size='lg' id='form2' type='email'/>
-          <MDBInput {...register("adress")} wrapperClass='mb-4' label='Your Adress' size='lg' id='form3' type='text'/>
-          <MDBInput {...register("phone_number")} wrapperClass='mb-4' label='Your phone number' size='lg' id='form4' type='number'/>
-          <h4 className="text text mb-5">Products</h4>
-          <MDBInput {...register("product")} wrapperClass='mb-4' label='Product' size='lg' id='form1' type='text'/>
-          <MDBInput {...register("price")} wrapperClass='mb-4' label='Unit Price' size='lg' id='form1' type='text'/>
-          <MDBBtn type="submit" className='mb-4 w-100 gradient-custom-4' size='lg'>Register</MDBBtn>
+            <Form.Group>
+              <FloatingLabel controlId="name" label="Name" className="mb-3">
+                <Form.Select
+                  className="mb-4"
+                  {...register("name")}
+                  onChange={onHandleChange}
+                  defaultValue={"default"}
+                >
+                  <option value="default" disabled>Select a customer</option>
+                  {customers.map((customer) => (
+                    <option key={customer.id}>{customer.name}</option>
+                  ))}
+                </Form.Select>
+              </FloatingLabel>
+            </Form.Group>
+            <Form.Group>
+              <FloatingLabel
+                controlId="lastname"
+                label="Lastname"
+                className="mb-3"
+              >
+                <Form.Control
+                  className="mb-4"
+                  {...register("lastname")}
+                  type="text"
+                  disabled
+                />
+              </FloatingLabel>
+            </Form.Group>
+            <Form.Group>
+              <FloatingLabel controlId="email" label="Email" className="mb-3">
+                <Form.Control
+                  className="mb-4"
+                  {...register("email")}
+                  type="email"
+                  disabled
+                />
+              </FloatingLabel>
+            </Form.Group>
+            <Form.Group>
+              <FloatingLabel
+                controlId="address"
+                label="Address"
+                className="mb-3"
+              >
+                <Form.Control
+                  className="mb-4"
+                  {...register("address")}
+                  type="text"
+                  disabled
+                />
+              </FloatingLabel>
+            </Form.Group>
+            <Form.Group>
+              <FloatingLabel
+                controlId="phone"
+                label="Phone Number"
+                className="mb-3"
+              >
+                <Form.Control
+                  className="mb-4"
+                  {...register("phone")}
+                  type="number"
+                  disabled
+                />
+              </FloatingLabel>
+            </Form.Group>
+            <h4 className="text text mb-5">Invoice Items</h4>
+            {fields.length !== 0 && (
+              <>
+                <Row className="mb-3">
+                  <Col className="font-weight-bold" lg={4}>
+                    Product
+                  </Col>
+                  <Col className="font-weight-bold" lg={3}>
+                    Quantity
+                  </Col>
+                  <Col className="font-weight-bold" lg={3}>
+                    Unit Price
+                  </Col>
+                  <Col lg={1}></Col>
+                </Row>
+                {fields.map((field, index) => (
+                  <Row className="mb-3" key={field.id}>
+                    <Col lg={4}>{field.name}</Col>
+                    <Col lg={3}>{field.quantity}</Col>
+                    <Col lg={3}>{field.value}</Col>
+                    <Col lg={1}>
+                      <Button variant="danger" onClick={() => remove(index)}>
+                        x
+                      </Button>
+                    </Col>
+                  </Row>
+                ))}
+              </>
+            )}
+            <Row className="mb-3">
+              <Col lg={4}>
+                <Form.Group>
+                  <FloatingLabel controlId="floatingInput" label="Product Name">
+                    <Form.Control
+                      placeholder="name"
+                      {...register("product.name")}
+                      type="text"
+                    />
+                  </FloatingLabel>
+                </Form.Group>
+              </Col>
+              <Col lg={3}>
+                <Form.Group>
+                  <FloatingLabel
+                    controlId="quantity"
+                    label="Qty"
+                    htmlFor="product-quantity"
+                    className="mb-3"
+                  >
+                    <Form.Control
+                      placeholder="1"
+                      {...register("product.quantity")} 
+                      type="number"
+                    />
+                  </FloatingLabel>
+                </Form.Group>
+              </Col>
+              <Col lg={3}>
+                <Form.Group>
+                  <FloatingLabel
+                    controlId="product-value"
+                    label="Price"
+                    className="mb-3"
+                  >
+                    <Form.Control
+                      placeholder="0"
+                      {...register("product.value")}
+                      type="number"
+                    />
+                  </FloatingLabel>
+                </Form.Group>
+              </Col>
+              <Col lg={1}>
+                <Button className="gradient-custom-4" onClick={() => onAddProduct(getValues("product"))}>
+                  +
+                </Button>
+              </Col>
+            </Row>
+            <Button
+              className="mb-4 w-100 gradient-custom-4"
+              size="lg"
+              type="submit"
+            >
+              Submit
+            </Button>
           </form>
-        </MDBCardBody>
-      </MDBCard>
-    </MDBContainer>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 }
 
